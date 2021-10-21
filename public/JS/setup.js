40f3638718823c4361560dd79e805d45152926e9
Philippe <philippepallone@gmail.com>
2021-10-21 14:12:09 +0200

    Bug Fix

--- public/JS/setup.js
+++ public/JS/setup.js
@@ -3,13 +3,24 @@ for (let h = 0; h < 24; h++) {
     Labels.push(h + ':00:00', h + ':15:00', h + ':30:00', h + ':45:00')
 }
 
+let datat = [];
+for (let v = 0; v < 96; v++) {
+    datat.push(4 + (Math.random() * 2))
+}
+
+let datah = [];
+for (let v = 0; v < 96; v++) {
+    datah.push(48 + (Math.random() * 2))
+}
+
+
 const TempData = {
     labels: Labels,
     datasets: [{
         label: 'Temp (°C)',
         backgroundColor: 'rgb(255, 0, 0)',
         borderColor: 'rgb(255, 0, 0)',
-        data: [5.12, 5.25, 5.09, 5.15, 5.20, 5.22, 5.19, 5.10],
+        data: datat,
     }]
 }
 
@@ -19,6 +30,6 @@ const HygroData = {
         label: 'Hygro (%)',
         backgroundColor: 'rgb(0, 0, 255)',
         borderColor: 'rgb(0, 0, 255)',
-        data: [48, 50, 49, 49, 47, 48, 49, 51],
+        data: datah,
     }]
 }
