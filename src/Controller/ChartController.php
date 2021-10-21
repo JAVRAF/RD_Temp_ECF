e40cc2a3a898ceb634c82a48611e3d4e108e5072
Philippe <philippepallone@gmail.com>
2021-10-21 10:54:15 +0200

    Bug Fix

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -17,52 +17,54 @@ class ChartController extends AbstractController
         $id = $this->getUser()->getid();
         $chambers = $coolingChamberRepository->findBystore($id);
 
+        $ajax = false;
+        if ($_GET) {
+            $q = $_GET['date'];
+            list($tp, $hp) = explode(",", $_GET['probe'], 2);
+
+            $con = mysqli_connect('127.0.0.1:3306', 'root', '', 'rdtemp_db');
+            if (!$con) {
+                die('Could not connect: ' . mysqli_error($con));
+            }
+
+            mysqli_select_db($con, "rdtemp_db");
+            $temp = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY) and probe = '" . $tp . "' ORDER BY date ASC;";
+            $hygro = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY)and probe = '" . $hp . "' ORDER BY date ASC;";
+            $tempresult = mysqli_query($con, $temp);
+            $hygroresult = mysqli_query($con, $hygro);
+
+            $tempvalue = array();
+            $hygrovalue = array();
+            if ($tempresult) {
+                $tempdata = mysqli_fetch_all($tempresult);
+                foreach ($tempdata as $val) {
+                    array_push($tempvalue, (integer)$val[3]);
+                    $ajax = true;
+                }
+            }
+            if ($hygroresult) {
+                $hygrodata = mysqli_fetch_all($hygroresult);
+                foreach ($hygrodata as $val) {
+                    array_push($hygrovalue, (integer)$val[3]);
+                    $ajax = true;
+                }
+            }
+            mysqli_close($con);
+
+            return $this->render('chart/chart.html.twig', [
+                'chambers' => $chambers,
+                'tempdata' => $tempdata,
+                'hygrodata' => $hygrodata,
+                'tempvalue' => $tempvalue,
+                'hygrovalue' => $hygrovalue,
+                'ajax' => $ajax
+
+            ]);
+        }
+
         return $this->render('chart/chart.html.twig', [
-            'chambers' => $chambers
-
-        ]);
-    }
-
-    /**
-     * @route("/displaychart")
-     */
-    public function showchart(): Response
-    {
-        $q = $_GET['date'];
-        list($tp, $hp) = explode(",", $_GET['probe'], 2);
-
-        $con = mysqli_connect('127.0.0.1:3306', 'root', '', 'rdtemp_db');
-        if (!$con) {
-            die('Could not connect: ' . mysqli_error($con));
-        }
-
-        mysqli_select_db($con, "rdtemp_db");
-        $temp = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY) and probe = '" . $tp . "' ORDER BY date ASC;";
-        $hygro = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY)and probe = '" . $hp . "' ORDER BY date ASC;";
-        $tempresult = mysqli_query($con, $temp);
-        $hygroresult = mysqli_query($con, $hygro);
-
-        $tempvalue = array();
-        $hygrovalue = array();
-        if ($tempresult) {
-            $tempdata = mysqli_fetch_all($tempresult);
-            foreach ($tempdata as $val) {
-                array_push($tempvalue, (integer)$val[3]);
-            }
-        }
-        if ($hygroresult) {
-            $hygrodata = mysqli_fetch_all($hygroresult);
-            foreach ($hygrodata as $val) {
-                array_push($hygrovalue, (integer)$val[3]);
-            }
-        }
-        mysqli_close($con);
-
-        return $this->render('chart/ajax.html.twig', [
-            'tempdata' => $tempdata,
-            'hygrodata' => $hygrodata,
-            'tempvalue' => $tempvalue,
-            'hygrovalue' => $hygrovalue
+            'chambers' => $chambers,
+            'ajax' => $ajax
         ]);
     }
 }
