72de501052628a80fa51c7f510dc9e6d68798354
Philippe <philippepallone@gmail.com>
2021-10-21 21:01:46 +0200

    Chart controller fix

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -22,12 +22,12 @@ class ChartController extends AbstractController
             $q = $_GET['date'];
             list($tp, $hp) = explode(",", $_GET['probe'], 2);
 
-            $con = mysqli_connect('127.0.0.1:3306', 'root', '', 'rdtemp_db');
+            $con = mysqli_connect('c8u4r7fp8i8qaniw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'kr3x31eyuu2mj853', 'putibko55spih8zd', 'j8knpia3omrkra47');
             if (!$con) {
                 die('Could not connect: ' . mysqli_error($con));
             }
 
-            mysqli_select_db($con, "rdtemp_db");
+            mysqli_select_db($con, "j8knpia3omrkra47");
             $temp = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY) and probe = '" . $tp . "' ORDER BY date ASC;";
             $hygro = "SELECT * FROM probe_data WHERE date >= '" . $q . "' AND date < ADDDATE('" . $q . "', INTERVAL 1 DAY)and probe = '" . $hp . "' ORDER BY date ASC;";
             $tempresult = mysqli_query($con, $temp);
